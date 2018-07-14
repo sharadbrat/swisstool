fa9d024d87e94ee13104c5c5d4c428597e857f48
Georgii Sharadze <georgy.sharadze.consultant@nielsen.com>
2018-07-14 15:10:43 +0300

    made inject and added all the env

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.scss';
+import { ParallaxDemo } from './component/parallax/ParallaxDemo'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ParallaxDemo>
+          <h1>Hello world</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum neque nisi voluptatum. Aperiam asperiores consequatur earum fuga itaque modi quibusdam reiciendis repudiandae vero voluptate? Amet earum odit perspiciatis recusandae voluptatum.</p>
+        </ParallaxDemo>
       </div>
     );
   }
---
+++ src/component/parallax/ParallaxDemo.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import './ParallaxDemo.scss'
+
+export function ParallaxDemo(props) {
+  return <div className="parallax-demo">{props.children}</div>;
+}
