7c78d6151f81646f6f43d2b0fd289d5caf8258ae
sharadbrat <sharoobrazno@mail.ru>
2018-07-17 18:35:45 +0300

    added default state

---
+++ src/store/defaultState.js
@@ -0,0 +1,18 @@
+export const defaultNotesState = {
+  notes: []
+};
+
+export const defaultTodolistState = {
+  todoItems: []
+};
+
+export const defaultDashboardState = {
+  notes: [],
+  todoItems: []
+};
+
+export const defaultState = {
+  notes: defaultNotesState,
+  todolist: defaultTodolistState,
+  dashboard: defaultDashboardState
+};
