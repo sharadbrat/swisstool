019a2b422540a58344a4320912bb84a081ccc93f
sharadbrat <sharoobrazno@mail.ru>
2018-08-22 15:58:15 +0300

    added store service instead of store by itself

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import { ConnectedRouter } from 'connected-react-router';
 import { Provider } from 'react-redux';
-import { createBrowserHistory } from 'history';
 
 import { Pages } from '$pages';
-import { configureStore } from '$store';
+import { StoreService } from '$service';
 
 import './App.scss';
 
-const history = createBrowserHistory();
 
 function App() {
   return (
-    <Provider store={configureStore(history)}>
-      <ConnectedRouter history={history}>
+    <Provider store={StoreService.obtainStore()}>
+      <ConnectedRouter history={StoreService.obtainHistory()}>
         <Pages/>
       </ConnectedRouter>
     </Provider>
