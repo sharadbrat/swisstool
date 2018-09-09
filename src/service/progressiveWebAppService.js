4f9481da5e5c48857f1f70f4cbec9e61dd24e5a5
sharadbrat <sharoobrazno@mail.ru>
2018-09-09 14:51:50 +0300

    added progressive web app service

---
+++ src/service/progressiveWebAppService.js
@@ -0,0 +1,36 @@
+import { StoreService } from '$service';
+import { globalSetInstallPromptActiveAction } from '$store';
+
+export const ProgressiveWebAppService = (() => {
+  let installPrompt;
+
+  const setInstallButtonActive = (active) => {
+    const store = StoreService.obtainStore();
+    store.dispatch(globalSetInstallPromptActiveAction({ isInstallButtonShown: active}));
+  };
+
+  return {
+    setupBeforeInstallDefferedPrompt: () => {
+      window.addEventListener('beforeinstallprompt', (e) => {
+        // Prevent Chrome 67 and earlier from automatically showing the prompt
+        e.preventDefault();
+        // Stash the event so it can be triggered later.
+        installPrompt = e;
+        // Set install button active
+        setInstallButtonActive(true);
+      });
+    },
+    showDefferedPrompt: () => {
+      installPrompt.prompt();
+      installPrompt.userChoice
+        .then((choiceResult) => {
+          if (choiceResult.outcome === 'accepted') {
+            setInstallButtonActive(false);
+          } else {
+            console.log('User dismissed the A2HS prompt');
+          }
+          installPrompt = null;
+        });
+    }
+  };
+})();
